7cc42842851e8f5187d23600525972a84ca34e2d
Billy Fortin <billy.fortin@vividseats.com>
2017-01-17 17:00:24 -0600

    Revert "Hardcode number of connections"

--- apache_exporter.go
+++ apache_exporter.go
@@ -156,7 +156,7 @@ func (e *Exporter) updateScoreboard(scoreboard string) {
 			e.scoreboard.WithLabelValues("open_slot").Inc()
 		}
 	}
-        e.scoreboard.WithLabelValues("total_processes").Add(150)
+        e.scoreboard.WithLabelValues("total_processes").Add(float64(len(scoreboard)))
 }
 
 func (e *Exporter) collect(ch chan<- prometheus.Metric) error {
